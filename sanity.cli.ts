94884c17fb84aa456f73207e635b0c37077ee17f
Milan <81045676+MilanBulatovic@users.noreply.github.com>
2026-04-22 03:55:34 +0200

    Updates

---
+++ sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "c3x5cnbq",
+    dataset: "production",
+  },
+  studioHost: "kinoqi",
+});
